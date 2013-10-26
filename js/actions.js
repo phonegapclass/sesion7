d70c9b5006eb9d977b3b8e9ba12e263de40fd3f1
Carlos Celma <solrac@igitsoft.com>
2013-10-26 17:25:46 -0500

    123

--- js/actions.js
+++ js/actions.js
@@ -116,8 +116,9 @@ function nuevoContacto(nom,tel,mail){
 function leerContactos(){
 	document.addEventListener("deviceready",function(){
 		function onSuccess(contacts) {
+			$('#lcontacto').html('');
 			for(i=0;i<contacts.length;i++){
-				$('#lcontacto').append('<li><a href="tel:'+contacts[i].phoneNumbers[0]+'">'+contacts[i].name.formatted+'</a></li>');
+				$('#lcontacto').append('<li><a href="tel:'+contacts[i].phoneNumbers[0].value+'">'+contacts[i].name.formatted+'</a></li>');
 			}
 		};
 		
